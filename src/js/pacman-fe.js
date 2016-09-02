59205a568066ef997364b62dcb80599a40f804d4
Ray Vafa <Ray.Vafa@domain.com.au>
2016-09-02 17:06:07 +1000

    add a todo

--- src/js/pacman-fe.js
+++ src/js/pacman-fe.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import PropertyDescriptionAnnotator from './property-description-annotator.js';
 
 const pacmanFE = () => {
+  // todo: this whole thing should only happen after property description is in the page
+  // todo: also use a better div className
   const entryPoint = document.querySelector('.fe-co-pacman-fe-entry-point');
   ReactDOM.render(
     <PropertyDescriptionAnnotator />,
